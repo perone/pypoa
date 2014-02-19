bf81d0f11e222dded163982bdd1304f27cc51929
Christian S. Perone <christian.perone@gmail.com>
2014-02-19 19:41:28 -0300

    Importação inicial.

---
+++ docpad.js
@@ -0,0 +1,193 @@
+module.exports = {
+
+  prompts: false,
+
+  // These are variables will be accessible via our templates
+  templateData: {
+
+    // Conference info
+    conf: {
+      name: "PyPOA",
+      description: "1o Python Porto Alegre",
+      date: "Junho 7",
+      // If your event is free, just comment this line
+      price: "R$ 10",
+      venue: "Falta Definir",
+      address: "Av. Carlos Gomes, 1000",
+      city: "Porto Alegre",
+      state: "Rio Grande do Sul"
+    },
+
+    // The Call To Action button at the header,
+    // If you don't want this, just remove the callToAction property.
+    callToAction: {
+        text: "Registre-se agora!",
+        link: "http://eventick.com.br"
+    },
+
+    // "Fork me on GitHub", if you don't want this, just remove the forkButton property
+    forkButton: {
+        repository: "https://github.com/braziljs/conf-boilerplate"
+    },
+
+    // Site info
+    site: {
+      theme: "yellow-swan",
+      url: "http://braziljs.github.io/conf-boilerplate/",
+      googleanalytics: "UA-33600000-0"
+    },
+
+    // Active sections on the website
+    // to deactivate comment out with '//'
+    // you can also change order here and it will reflect on page
+    sections: [
+      'about',
+      'location',
+      'speakers',
+      'schedule',
+      'sponsors',
+      'partners'
+      // 'contact'
+    ],
+
+    // Labels which you can translate to other languages
+    labels: {
+      about: "Sobre",
+      location: "Lugar",
+      speakers: "Palestrantes",
+      schedule: "Programação",
+      sponsors: "Patrocinadores",
+      partners: "Parceiros",
+      contact: "Contato"
+    },
+
+    // The entire schedule
+    schedule: [
+      {
+        name: "Check-in / Breakfast",
+        time: "9h00"
+      },
+      {
+        name: "Christian S. Perone",
+        photo: "http://www.gravatar.com/avatar/7735d49d1b04ba5e18bbe107ad756b0d?s=200",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+        company: "Linux Foundation",
+        link: {
+          href: "http://twitter.com/linus",
+          text: "@linus"
+        },
+        presentation: {
+          title: "Machine Learning com Python",
+          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+          time: "10h00"
+        }
+      },
+      {
+        name: "Bill Gates",
+        photo: "themes/yellow-swan/img/speaker.jpg",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+        company: "Microsoft",
+        link: {
+          href: "http://github.com/billy95",
+          text: "@billy95"
+        },
+        presentation: {
+          title: "Introducing Windows 12",
+          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+          time: "11h00"
+        }
+      },
+      {
+        name: "Lunch",
+        time: "12h00"
+      },
+      {
+        name: "Chuck Norris",
+        photo: "themes/yellow-swan/img/speaker.jpg",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+        company: "Delta Command",
+        link: {
+          href: "http://twitter.com/littlechuck",
+          text: "@littlechuck"
+        },
+        presentation: {
+          title: "How to kill a elephant with one finger",
+          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+          time: "13h00"
+        }
+      },
+      {
+        name: "Steve Jobs",
+        photo: "themes/yellow-swan/img/speaker.jpg",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+        company: "Apple, Inc.",
+        link: {
+          href: "http://github.com/stevie",
+          text: "@stevie"
+        },
+        presentation: {
+          title: "Presenting iPad",
+          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+          time: "14h00"
+        }
+      },
+      {
+        name: "Coffee-break",
+        time: "15h00"
+      },
+      {
+        name: "Mark Zuckerberg",
+        photo: "themes/yellow-swan/img/speaker.jpg",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+        company: "Facebook",
+        link: {
+          href: "http://twitter.com/zuck",
+          text: "@zuck"
+        },
+        presentation: {
+          title: "Revealing Facebook Secrets",
+          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+          time: "16h00"
+        }
+      },
+      {
+        name: "Steve Wozniak",
+        photo: "themes/yellow-swan/img/speaker.jpg",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+        company: "Apple, Inc.",
+        link: {
+          href: "http://twitter.com/woz",
+          text: "@woz"
+        },
+        presentation: {
+          title: "Why do I prefer Android over iPhone",
+          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+          time: "17h00"
+        }
+      }
+    ],
+
+    // List of Sponsors
+    sponsors: [
+      {
+        name: "Eventick",
+        logo: "themes/yellow-swan/img/sponsor.png",
+        url: "http://eventick.com.br"
+      }
+    ],
+
+    // List of Partners
+    partners: [
+      {
+        name: "BrazilJS",
+        logo: "themes/yellow-swan/img/partner.png",
+        url: "http://braziljs.org"
+      }
+    ],
+
+    // Theme path
+    getTheme: function() {
+      return "themes/" + this.site.theme;
+    }
+  }
+};
